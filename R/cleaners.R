9df2e5589550bb36e445b2c52c434e3ca1047779
Rodrigo Viale <rodrigogarciaviale@gmail.com>
2025-02-17 23:51:04 +0100

    Added fromto_scale function. Added comments. Added arguments to raw_to_basf.

--- R/cleaners.R
+++ R/cleaners.R
@@ -1,22 +1,54 @@
-raw_to_basf <- function(.data){
+raw_to_basf <- function(.data,
+                        .long_cols = c("code", "px"),
+                        .sep_delim = "(",
+                        .sep_into  = c("code", "side"),
+                        .bid_id    = "EB)",
+                        .fwd_end   = c("F", "1MFP", "1M")){
+  #   This function takes the raw data from DS and formats it into a long tibble with date, code, side, px, mkt
+  #   Input: Codes of time series as headers, dates in the first column. Series for bids, asks, spots and forwards.
   
   .data |> 
-    pivot_longer(
+    tidyr::pivot_longer( # Pivot into code column for DS codes (includes suffix for bid/ask) and px for prices.
       -date,
-      names_to  = "code",
-      values_to = "px"
+      names_to  = .long_cols[1],
+      values_to = .long_cols[2]
     ) |> 
-    separate_wider_delim(
+    tidyr::separate_wider_delim( # DS provides the time series with format X(EB)/X(EO), where X is the DS code, and 
+      # EB/EO are identifiers for bids and asks, respectively. Separate bid/ask identifier into new column.
       col   = code,
-      delim = "(",
-      names = c("code", "side")
+      delim = .sep_delim,
+      names = .sep_into
+    ) |> 
+    dplyr::mutate(
+      date = lubridate::as_date(date), # Convert from datetime to date (since I use monthly data, time is not relevant).
+      side = dplyr::if_else(side == .bid_id, "bid", "ask"), # Change bid/ask identifier column.
+      mkt = dplyr::if_else( # New column identifying forwards/spots by suffix: only F, 1MFP or 1M are forwards.
+        stringr::str_detect(code, "(F|1MFP|1M)$"),
+        "fwd",
+        "spot"
+      )
+    )
+  
+}
+
+
+fromto_scale <- function(.data,
+                         .fromto,
+                         .by_col,
+                         .scale_col){
+  #   This function joins the quotes data with the table containing information on the from/to and the spreads scales.
+  
+  .data |> 
+    left_join(
+      y            = .fromto,
+      by           = join_by( {{ .by_col }} ),
+      relationship = "many-to-one" # Each row in df has to match at most 1 row in fromto.
     ) |> 
     mutate(
-      date = as_date(date),
-      side = if_else(side == "EB)", "bid", "ask") |> as.factor(),
-      mkt  = if_else(str_ends(code, "F") | str_ends(code, "1MFP") | str_ends(code, "1M"),
-                     "fwd",
-                     "spot") |> as.factor()
-    )
+      px  = ifelse(!is.na( {{ .scale_col }} ), px / {{ .scale_col }}, px), # Scale spreads according to scale (outrights are NA).
+      mkt = ifelse(!is.na( {{ .scale_col }} ), "spr", mkt) # Identify spreads in mkt column. After this, mkt column contains 
+      # 3 identifiers: spot, spr (spread quotes) and fwd (outright quotes)
+    ) |> 
+    select(-c( {{ .by_col }}, {{ .scale_col }} )) # Drop name and scale column
   
 }
