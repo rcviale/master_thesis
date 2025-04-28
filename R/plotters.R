c281c544093cab33146e722fcd6a8b05311aa16c
Rodrigo Viale <rodrigogarciaviale@gmail.com>
2025-04-28 22:06:16 +0200

    Initial commit.

---
+++ R/plotters.R
@@ -0,0 +1,46 @@
+group_line_plot <- function(.data){
+  # This function makes a line plot for each group (time series in this case).
+  
+  p <- .data |> 
+    ggplot2::ggplot(ggplot2::aes(x = date, y = px, color = label)) +  # Initialize ggplot with aesthetics mapping
+    ggplot2::geom_line(linewidth = 1) +  # Add lines for each group with specified line width
+    ggplot2::labs(title = unique(.data$from)) +  # Set the plot title based on the 'from' column
+    ggplot2::theme_minimal()  # Apply a minimalistic theme to the plot
+  
+  p |> 
+    plotly::ggplotly() |>  # Convert ggplot object to interactive Plotly object
+    print()  # Print the interactive plot
+  
+}
+
+
+
+
+simple_line <- function(.data,
+                        .x,
+                        .y,
+                        .title = NULL,
+                        .xlab  = NULL,
+                        .ylab  = NULL,
+                        .col   = "red"){
+  #   This function generates a simple line plotly with nice formatting.
+  
+  p <- .data |>
+    ggplot2::ggplot() +  # Initialize a ggplot object with the input data
+    ggplot2::geom_line(  # Add a line geometry to the plot
+      ggplot2::aes(x = {{ .x }}, y = {{ .y }}),  # Map aesthetics: x and y variables
+      col       = "red",     # Set line color
+      linewidth = 1          # Set line width
+    ) +
+    ggplot2::labs(           # Add labels: title, x-axis label, y-axis label
+      title = ifelse(is.null(.title), paste0("Line Plot of ", rlang::enexpr(.y), " over ", rlang::enexpr(.x)), .title),
+      x     = ifelse(is.null(.xlab), paste0(rlang::enexpr(.x)), .xlab),
+      y     = ifelse(is.null(.ylab), paste0(rlang::enexpr(.y)), .ylab)
+    ) + 
+    ggplot2::theme_bw()      # Apply a black and white theme to the plot
+  
+  p |>
+    plotly::ggplotly() |>    # Convert the ggplot object to an interactive Plotly plot
+    print()                  # Print the interactive plot
+}
+
---
+++ R/startup.R
@@ -0,0 +1,4 @@
+if (!require("tidyverse")) install.packages("tidyverse"); library("tidyverse")
+if (!require("frenchdata")) install.packages("frenchdata"); library("frenchdata")
+if (!require("readxl")) install.packages("readxl"); library("readxl")
+if (!require("plotly")) install.packages("plotly"); library("plotly")
