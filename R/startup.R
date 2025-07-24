6644b433f7ce02f1b4bcbc4825e3b03ef206a6f3
Rodrigo Viale <rodrigogarciaviale@gmail.com>
2025-07-24 16:06:03 +0200

    Minor changes.

--- R/startup.R
+++ R/startup.R
@@ -1,5 +1,4 @@
 if (!require("tidyverse")) install.packages("tidyverse"); library("tidyverse")
-if (!require("frenchdata")) install.packages("frenchdata"); library("frenchdata")
 if (!require("readxl")) install.packages("readxl"); library("readxl")
 if (!require("plotly")) install.packages("plotly")
 if (!require("slider")) install.packages("slider")
